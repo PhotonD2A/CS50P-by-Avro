dc4a235943b4cfb4cc8b972eeb3a87bd5bd39114
PhotonD2A <166201380+PhotonD2A@users.noreply.github.com>
2024-11-18 17:22:10 +0600

    Update meal.py

--- Week-1/Meal/meal.py
+++ Week-1/Meal/meal.py
@@ -1,1 +1,19 @@
+def main():
+    time = input("What time is it? ")
+    timeCheck = convert(time)
 
+    if 7.0 <= timeCheck <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= timeCheck <= 13.0:
+        print("lunch time")
+    elif 18.0 <= timeCheck <= 19.0:
+        print("dinner time")
+
+def convert(time):
+    hours, minutes = time.split(":")
+    hours = float(hours)
+    minutes = float(minutes) / 60
+    return hours + minutes
+
+if __name__ == "__main__":
+    main()
