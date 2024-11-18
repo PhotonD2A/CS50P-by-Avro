552f189458b7c8c8815699af3bb0f1a3c137bd76
ALimul AVro <166201380+PhotonD2A@users.noreply.github.com>
2024-11-18 19:10:10 +0600

    Update coke.py

--- Week-2/Coke Machine/coke.py
+++ Week-2/Coke Machine/coke.py
@@ -1,18 +1,18 @@
 def main():
     amount_due = 50
-    print(f"Amount due: {amount_due}")
+    print(f"Amount Due: {amount_due}")
 
     while amount_due > 0:
-        coin = int(input("Insert coin: "))
+        coin = int(input("Insert Coin: "))
 
         if coin in [25, 10, 5]:
             amount_due -= coin
             if amount_due > 0:
-                print(f"Amount due: {amount_due}")
+                print(f"Amount Due: {amount_due}")
         else:
-            print(f"Amount due: {amount_due}")
+            print(f"Amount Due: {amount_due}")
 
-    print(f"Change owed: {abs(amount_due)}")
+    print(f"Change Owed: {abs(amount_due)}")
 
 if __name__ == "__main__":
     main()
